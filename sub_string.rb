cea9578d00fe6e954a34dfd918040ef777174ee1
Felippe Santana <felippemenezes1982@gmail.com>
2026-04-19 12:48:20 -0300

    #scan method is used to check how many times the word was found in the string. And #count used to transform number of occurrences into an integer.

--- sub_string.rb
+++ sub_string.rb
@@ -1,14 +1,19 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own", "lo","part","partner","sit"]
 
 def substrings ( string, dictionary)
-  p string
-  p dictionary
+  hash = Hash.new(0)
+  downcase_string = string.downcase
+  p downcase_string
 
-  dictionary.each do | word |
-    if string.include?(word)
-      p word
+  dictionary.each do |word|
+    p word
+    p string.scan(/#{word}/).count
+    if string.scan(/#{word}/).count != 0
+      hash[word] = string.scan(/#{word}/).count
+      p hash
     end
   end
 end
 
-substrings( "below", dictionary)
+# substrings( "below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
