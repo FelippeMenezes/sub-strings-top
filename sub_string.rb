1cbbef212eeae0655509362c7728a64b8149062a
Felippe Santana <felippemenezes1982@gmail.com>
2026-04-19 10:47:07 -0300

    Created hash with string key and array value

--- sub_string.rb
+++ sub_string.rb
@@ -1,7 +1,8 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings ( string, dictionary)
-
+  hash = { "#{string}": dictionary }
+  hash
 end
 
-p substrings
+p substrings( "low", dictionary)
