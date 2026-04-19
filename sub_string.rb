81546112f6adf2c830a6e5cbbc519f69bc5025fe
Felippe Santana <felippemenezes1982@gmail.com>
2026-04-19 15:03:24 -0300

    #downcase applied to user inputs.

--- sub_string.rb
+++ sub_string.rb
@@ -1,21 +1,20 @@
 def substrings ( string, dictionary)
   hash = Hash.new(0)
-  downcase_string = string.downcase
 
   dictionary.each do |word|
-    if downcase_string.scan(/#{word}/).count != 0
-      hash[word] = downcase_string.scan(/#{word}/).count
+    if string.scan(/#{word}/).count != 0
+      hash[word] = string.scan(/#{word}/).count
     end
   end
+
   puts hash
 end
 
 puts "Please, write your amazing sentence below."
-user_string = gets.chomp
+user_string = gets.chomp.downcase
 
-puts "Now, include the words for which you want to count the occurrences in the sentence, separated by ' ' (empty space)."
-user_words = gets.chomp
-user_dictionary = user_words.split(" ")
+puts "Now, type the subwords, separated by ' ' (empty space), that you want to count how many times they appear in the sentence."
+user_dictionary = gets.chomp.downcase.split(" ")
 
 substrings(user_string, user_dictionary)
 
