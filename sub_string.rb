4e14c6815ec41cfee2a453fffde47f5c980117ac
Felippe Santana <felippemenezes1982@gmail.com>
2026-04-19 11:10:17 -0300

    Parameter name 'substring' changed to 'string'

--- sub_string.rb
+++ sub_string.rb
@@ -1,8 +1,9 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substrings ( substring, dictionary)
-  p substring
+def substrings ( string, dictionary)
+  p string
   p dictionary
+
 end
 
 substrings( "low", dictionary)
