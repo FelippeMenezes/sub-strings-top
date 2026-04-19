c41816cd1ea2ec67bcffc7fe90a6024581964653
Felippe Santana <felippemenezes1982@gmail.com>
2026-04-19 13:58:23 -0300

    User input message and dictionary user input created

--- sub_string.rb
+++ sub_string.rb
@@ -1,14 +1,10 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
 def substrings ( string, dictionary)
   hash = Hash.new(0)
   downcase_string = string.downcase
-  p downcase_string
 
   dictionary.each do |word|
     if downcase_string.scan(/#{word}/).count != 0
       hash[word] = downcase_string.scan(/#{word}/).count
-
     end
   end
   puts hash
@@ -17,5 +13,9 @@ end
 puts "Please, write your amazing sentence below."
 user_string = gets.chomp
 
-substrings(user_string, dictionary)
+puts "Now, include the words for which you want to count the occurrences in the sentence, separated by ' ' (empty space)."
+user_words = gets.chomp
+user_dictionary = user_words.split(" ")
+
+substrings(user_string, user_dictionary)
 
